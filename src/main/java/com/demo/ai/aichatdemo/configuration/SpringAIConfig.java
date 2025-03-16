e819727209f101cb174d50255c75cf1c379d0edf
pks9862728888 <pks9862728888@gmail.com>
2025-03-16 13:13:49 +0530

    Added custom user advice

--- src/main/java/com/demo/ai/aichatdemo/configuration/SpringAIConfig.java
+++ src/main/java/com/demo/ai/aichatdemo/configuration/SpringAIConfig.java
@@ -16,6 +16,21 @@ import org.springframework.context.annotation.Configuration;
 public class SpringAIConfig {
   private final VectorStore vectorStore;
 
+  private static final String CUSTOM_USER_TEXT_ADVISE = """
+			
+			Context information is below, surrounded by ---------------------
+
+			---------------------
+			{question_answer_context}
+			---------------------
+			
+			You are an expert in writing dsl function and highly trained to provide valuable answers from context.
+
+			Given the context and provided history information and not prior knowledge,
+			reply to the user comment. If the answer is not in the context, inform
+			the user that you can't answer the question. Try to provide examples in your answer.
+			""";
+
   @Bean
   public ChatMemory chatMemory() {
     return new InMemoryChatMemory();
@@ -28,7 +43,8 @@ public class SpringAIConfig {
             new MessageChatMemoryAdvisor(chatMemory),
             new QuestionAnswerAdvisor(vectorStore, SearchRequest.builder()
                 .topK(10)
-                .build())
+                .similarityThreshold(0.5f)
+                .build(), CUSTOM_USER_TEXT_ADVISE)
         )
         .build();
   }
